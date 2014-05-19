7fd94db0d1e268de0ff7fcbddb4d8fb111b4f44e
Chandima Prematillake <cprematillake@navinet.net>
2014-05-19 15:34:16 -0400

    Extracted OutputWindow into a new sub-module robot-common.py

---
+++ lib/robot_common.py
@@ -0,0 +1,25 @@
+import os
+import re
+import sublime
+
+#--------------------------------------------------------------------------
+# This is the class that outputs test progress and results into a window
+#--------------------------------------------------------------------------
+class OutputWindow():
+    def __init__(self, window, name):
+
+        self.console = window.new_file()
+        self.console.set_name(name)
+
+        self.console.set_scratch(True)
+        self.console.set_read_only(True)
+        self.console.set_syntax_file(os.path.join(plugin_dir, 'robot-output.tmLanguage'))
+
+    def append_text(self, output):
+
+        self.console.set_read_only(False)
+        edit = self.console.begin_edit()
+        self.console.insert(edit, self.console.size(), output)
+        self.console.end_edit(edit)
+        self.console.set_read_only(True)
+
