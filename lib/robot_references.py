2986f20ce9b8359b43f814b32fc88b35f2ee6f67
Chandima Prematillake <cprematillake@navinet.net>
2014-05-21 16:00:16 -0400

    Refactored the find references code

--- lib/robot_references.py
+++ lib/robot_references.py
@@ -15,49 +15,55 @@ class FindReferencesService():
         self.view = view
         self.edit = edit
         self.plugin_dir = plugin_dir
-
+        self.window = sublime.active_window()
+        self.results_to_display = []
+        self.references = []
+      
     def find(self):
-        view = self.view
-        keyword = LineAtCursor(view).get_keyword()
+        keyword = LineAtCursor(self.view).get_keyword()
         
         if not keyword:
             sublime.error_message('No keyword detected')
             return	
                 
-        self.window = sublime.active_window()
-      
-        listItems = []
-        self.matchingLines = []
-        for folder in view.window().folders():
-            for root, dirs, files in os.walk(folder):
-                for f in files:
-                    if is_robot_file(f) and f != '__init__.txt':
-                        path = os.path.join(root, f)
-                        try:
-                            with open(path, 'rb') as openFile:
-                                lines = openFile.readlines()
-                                lineNumber = 0 
-                                for aLine in lines:
-                                    lineNumber = lineNumber + 1
-                                    try:
-                                        if keyword in str(aLine):
-                                            matchingLine= MatchingFile(aLine.strip(),str(f),path, lineNumber)
-                                            self.matchingLines.append(matchingLine)
-                                            listItems.append(matchingLine.fileName + ': #' + str(matchingLine.lineNumber) + ' - '+ matchingLine.lineText)
-                                    except Exception as exp:
-                                        print('Issue in file ' +str(f) + ' line number ' + str(lineNumber) + ': ' +exp.message)
-                        except IOError as e:
-                            return
-        
-        def on_done(i):
-            newView = self.window.open_file(self.matchingLines[i].filePath + ':' + str(self.matchingLines[i].lineNumber), sublime.ENCODED_POSITION)
-            self.window.focus_view(newView)
-            pt = newView.text_point(self.matchingLines[i].lineNumber-1, 0)
-            newView.sel().clear()
-            newView.sel().add(sublime.Region(pt))
-            newView.show(pt)
+        self._search_within_folder(keyword)
+        self.window.show_quick_panel(self.results_to_display, self._on_user_select, sublime.MONOSPACE_FONT)
 
-        self.window.show_quick_panel(listItems, on_done, sublime.MONOSPACE_FONT)
+    def _search_within_folder(self, phrase):
+        for folder in self.view.window().folders():
+            for root, dirs, files in os.walk(folder):
+                for file in files:
+                    if is_robot_file(file):
+                        self._search_within_file(root, file, phrase)
+
+    def _search_within_file(self, root, file_name, phrase):
+        file_path = os.path.join(root, file_name)
+        try:
+            with open(file_path, 'rb') as file:
+                lines = file.readlines()
+                line_number = 0
+                for a_line in lines:
+                    line_number = line_number + 1
+                    try:
+                        if phrase in str(a_line):
+                            reference = ReferencedLine(a_line.strip(), str(file_name), file_path, line_number)
+                            self.references.append(reference)
+                            self.results_to_display.append(reference.to_display())
+
+                    except Exception as exp:
+                        print('Issue in file: ' + str(file_path) + ' line: ' + str(line_number) + ': ' + str(exp))
+
+        except IOError as e:
+            return
+                
+    def _on_user_select(self, index):
+        if index != -1:
+            new_view = self.window.open_file(self.references[index].link(), sublime.ENCODED_POSITION)
+            self.window.focus_view(new_view)
+            pt = new_view.text_point(self.references[index].line_number - 1, 0)
+            new_view.sel().clear()
+            new_view.sel().add(sublime.Region(pt))
+            new_view.show(pt)
 
 #------------------------------------------------------
 # 
@@ -110,20 +116,20 @@ class References1():
                         try:
                             with open(path, 'rb') as openFile:
                                 lines = openFile.readlines()
-                                lineNumber = 0 
+                                line_number = 0 
                                 for aLine in lines:
-                                    lineNumber = lineNumber + 1
+                                    line_number = line_number + 1
                                     try:
                                         if oldKeyword in str(aLine):
-                                            #matchingKeyword= MatchingFile(aLine.strip(),str(f),path, lineNumber)
+                                            #matchingKeyword= ReferencedLine(aLine.strip(),str(f),path, line_number)
                                             if output_window is not None:
                                                 if firstReplace == 1:
                                                     output_window.append_text('In file \''+str(f) +'\'\n')
                                                     firstReplace = 0
-                                                output_window.append_text('Line ' +str(lineNumber) + ' - Replacing ' + aLine.strip() + '\n\n')
+                                                output_window.append_text('Line ' +str(line_number) + ' - Replacing ' + aLine.strip() + '\n\n')
                                                 replaceCount = replaceCount+1
                                     except Exception as exp:
-                                        print('Issue in file ' +str(f) + ' line number ' + str(lineNumber) + ': ' +exp.message)
+                                        print('Issue in file ' +str(f) + ' line number ' + str(line_number) + ': ' +exp.message)
                         except IOError as e:
                             return
                     
@@ -133,14 +139,19 @@ class References1():
             if output_window is not None:
                     output_window.append_text('\nTotal ' + str(replaceCount) + ' occurrences replaced')
 
-#------------------------------------------------------
-# 
-#------------------------------------------------------
+#----------------------------------------------------------------
+# Represents a single reference to a phrase (keyword/variable)
+#----------------------------------------------------------------
 
-class MatchingFile:
-    def __init__(self, lineText, fileName, filePath, lineNumber):
-        self.lineText = lineText
-        self.fileName = fileName
-        self.filePath = filePath
-        self.lineNumber = lineNumber
+class ReferencedLine:
+    def __init__(self, line_text, file_name, file_path, line_number):
+        self.line_text = line_text
+        self.file_name = file_name
+        self.file_path = file_path
+        self.line_number = line_number
 
+    def to_display(self):
+        return self.file_path + '(' + str(self.line_number) + '): '+ self.line_text
+
+    def link(self):
+        return self.file_path + ':' + str(self.line_number)
